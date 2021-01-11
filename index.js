c3211b2ab56e60f12ecc145a608ae37d0dbdf197
dainbrownlow08 <dain.brownlow@outlook.com>
2021-01-11 11:50:11 -0800

    commit tree a18ee7f37efd81a2d1c835118b4d1a1499149b07

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(aOSN,eN){
+    var a = [];
+    for(let i = 0; i < aOSN.length; i++){
+        a.push(`Thank you, ${aOSN[i]}, for the wonderful ${eN} gift!`);
+    }
+    return a;
+}
+
+function countDown(n){
+    for(let i = n; i >= 0 ; i--){
+        console.log(i);
+    }
+}
